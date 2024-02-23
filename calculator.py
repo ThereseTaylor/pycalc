b1729ad23950c5ab02a55c42863ccf02de547bf4
Stefan Pretorius <86552342+StefanPretorius@users.noreply.github.com>
2024-02-23 09:33:26 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,11 +1,14 @@
 def add(x: float, y: float) -> float:
     return x + y
 
+
 def subtract(x: float, y: float) -> float:
     return x - y
 
+
 def multiply(x: float, y: float) -> float:
     return x * y
+
 
 def divide(x: float, y: float) -> float:
     if y == 0:
