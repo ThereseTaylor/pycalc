f19d310dda3be03c7706edafd4fd2c797bb0363b
Therese Taylor <112098539+ThereseTaylor@users.noreply.github.com>
2024-02-25 18:57:15 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,3 +14,11 @@ def divide(x: float, y: float) -> float:
     if y == 0:
         raise ValueError("Cannot divide by zero")
     return x / y
+
+def main():
+    while True:
+        x = int(input("Enter a number: "))
+        y = int(input("Enter a number: "))
+
+if __name__ == "__main__":
+    main()
