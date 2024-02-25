17c81ca1f807f8f94c96d2297dd87885367b6a67
Kyle Kumm <kkumm16@gmail.com>
2024-02-25 17:06:41 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -14,3 +14,12 @@ def divide(x: float, y: float) -> float:
     if y == 0:
         raise ValueError("Cannot divide by zero")
     return x / y
+
+
+def main():
+    while(True):
+        add(1,1)
+
+
+if __name__ == "__main__":
+    main()
